08e00338c3cf12b152dfd482f173a38229dc1eaa
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2022-07-10 10:17:32 +0530

    add cal-popup, send low bat notification

--- scripts/low-bat-notification.py
+++ scripts/low-bat-notification.py
@@ -6,8 +6,5 @@ batt_status = subprocess.check_output(["cat", "/sys/class/power_supply/BAT0/stat
 if capacity < 10 and batt_status == b'Discharging\n':
     subprocess.call(["sh", "/usr/local/bin/suspend-without-sudo.sh"])
 elif capacity < 15 and batt_status == b'Discharging\n':
-    subprocess.run(["ffplay", "-f", "lavfi", "-i", "sine=frequency=1000:duration=1", "-autoexit", "-nodisp"])
-    p1 = subprocess.Popen(['echo', 'low battery'], stdout=subprocess.PIPE)
-    p2 = subprocess.check_output(["aosd_cat", "-p", "0", "-t", "0", "-f", "0", "-y", "-1", "-x", "5", "-R", "red", "-n", '"Cascadia Code"'], stdin=p1.stdout)
-    p1.wait()
+    subprocess.run(["notify-send", "low battery"])
 
